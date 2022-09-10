372eebf6edf193bb207c429e1875cb1c300426f0
tushar <tushar.khan0122@gmail.com>
2022-09-10 20:16:58 +0600

    manage README.md

--- tusharKhan/Faker/Lib/Utils.php
+++ tusharKhan/Faker/Lib/Utils.php
@@ -13,21 +13,5 @@ use Tusharkhan\BanglaFaker\BanglaFaker;
 
 class Utils extends BanglaFaker
 {
-//    public static function banglaDate()
-//    {
-//        $arguments = func_get_args();
-//        $format = 'd-m-Y H:i:s';
-//
-//        $date = Carbon::now()->format($format);
-//
-//        if( count($arguments) > 0 ){
-//            $date = ( count($arguments[0]) > 0 && $arguments[0][0] != null ) ? $arguments[0][0] : $date;
-//            $format = ( count($arguments[0]) > 1 && $arguments[0][1] != null ) ? $arguments[0][1] : $format;
-//        }
-//
-//        if( $date ) $date = Carbon::parse($date)->format($format);
-//
-//
-//        return Number::getBanglaNumber($date);
-//    }
+    //
 }
